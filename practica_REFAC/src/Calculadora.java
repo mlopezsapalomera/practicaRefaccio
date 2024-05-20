9e743be050b142248bf88ae889d507b19e10a29c
Marcos López Medina <164485513+mlopezsapalomera@users.noreply.github.com>
2024-05-20 11:17:15 +0200

    Create Calculadora.java

---
+++ practica_REFAC/src/Calculadora.java
@@ -0,0 +1,17 @@
+public class Calculadora {
+    public static void calcEquacioSegongrau(double a, double b, double c) {
+        double D = b * b - 4 * a * c;
+        if (D > 0) {
+            double x1, x2;
+            x1 = (-b - Math.sqrt(D)) / (2 * a);
+            x2 = (-b + Math.sqrt(D)) / (2 * a);
+            System.out.println("x1 = " + x1 + ", x2 = " + x2);
+        } else if (D == 0) {
+            double x;
+            x = -b / (2 * a);
+            System.out.println("x = " + x);
+        } else {
+            System.out.println("La ecuación no tiene raíces reales");
+        }
+    }
+}
