e0e5f9a5086f987f25e0bff3f0fb1485a4e4fb74
Marcos López Medina <164485513+mlopezsapalomera@users.noreply.github.com>
2024-05-20 11:17:32 +0200

    Create Human.java

---
+++ practica_REFAC/src/Human.java
@@ -0,0 +1,23 @@
+public class Human {
+    private String name;
+    private String age;
+    private String country;
+    private String city;
+    private String street;
+    private String house;
+    private String quarter;
+
+    public String obtenirAdrecaCompleta() {
+        StringBuilder result = new StringBuilder();
+        return result.append(country)
+                     .append(", ")
+                     .append(city)
+                     .append(", ")
+                     .append(street)
+                     .append(", ")
+                     .append(house)
+                     .append(" ")
+                     .append(quarter)
+                     .toString();
+    }
+}
